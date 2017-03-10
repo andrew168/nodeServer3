ec56bb9608fa64b58881423875cbce9bd8b281b1
andrewz <andrewz@amayasoftware>
2017-03-10 16:46:20 -0500

    gulp测试

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,23 @@
-
 /*eslint strict: ["error", "global"]*/
 'use strict';
 
 var gulp = require('gulp');
+var $ = require('gulp-load-plugins')(); //jshint ignore:line
+var del = require('del');
+var runSequence = require('run-sequence');
+
+gulp.task('task1', function(callback) {
+    runSequence('task2', 'task3', callback);
+});
+
+gulp.task('task2', function () {
+    console.log("task2");
+});
+
+gulp.task('task3', function () {
+    console.log("task3");
+});
 
 gulp.task('default', function () {
-    console.log("gulp is working!");
+    gulp.start('task1');
 });
